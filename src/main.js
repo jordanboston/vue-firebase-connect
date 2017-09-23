a344fd699e0250aa2214dbd588e55a11fefd6cd4
Jordan Boston <jordandboston@gmail.com>
2017-09-23 15:22:13 -0700

    example of VueResource interceptor -commented out

--- src/main.js
+++ src/main.js
@@ -9,6 +9,15 @@ Vue.use(VueResource);
 // the 'root' key will set our main base url for this app
 Vue.http.options.root = 'https://vuejs-http-example-ef7a8.firebaseio.com/users.json';
 
+// example of interceptor (will change our request to a PUT request)
+// Vue.http.interceptors.push((request, next) => {
+// 	console.log(request);
+// 	if (request.method = 'POST') {
+// 		request.method = 'PUT';
+// 	}
+// 	next();
+// });
+
 new Vue({
   el: '#app',
   render: h => h(App)
